6582548aa015293c28af031b443571f41d4ed08b
vickyadi <vickyadi@synapsis.id>
2023-12-27 23:51:07 +0700

    feat: defautl theme in button

--- src/uikits/button/Button.tsx
+++ src/uikits/button/Button.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { ThemeProvider } from "../../providers/theme-providers";
+import { theme as defautTheme } from "../../tokens";
 
 type ButtonProps = {
   type?: "primary" | "secondary";
@@ -10,27 +10,39 @@ type ButtonProps = {
 
 const StyledButton = styled.button<ButtonProps>`
   background-color: ${(props) =>
-    props?.theme?.backgroundColors?.[props.type ?? "primary"]};
-  color: ${(props) => props?.theme?.colors?.[props?.type ?? "primary"]};
-  padding: ${(props) => props?.theme?.sizes?.[props?.size ?? "medium"]};
+    props?.theme?.backgroundColors?.[props.type || "primary"] ||
+    defautTheme.backgroundColors.primary};
+  color: ${(props) =>
+    props?.theme?.colors?.[props?.type || "primary"] ||
+    defautTheme.colors.primary};
+  padding: ${(props) =>
+    props?.theme?.sizes?.[props?.size || "medium"] || defautTheme.sizes.medium};
   border-radius: ${(props) =>
-    props?.theme?.borderRadius?.[props?.size ?? "medium"]};
-  border: ${(props) => props?.theme?.borderColors?.[props?.type ?? "primary"]};
+    props?.theme?.borderRadius?.[props?.size || "medium"] ||
+    defautTheme.borderRadius.medium};
+  border: ${(props) =>
+    props?.theme?.borderColors?.[props?.type || "primary"] ||
+    defautTheme.borderColors.primary};
 
   cursor: pointer;
   transition: background-color 0.3s ease;
 
   &:hover {
     background-color: ${(props) =>
-      props?.theme?.backgroundColorsHover?.[props?.type ?? "primary"]};
-    color: ${(props) => props?.theme?.colorsHover?.[props?.type ?? "primary"]};
+      props?.theme?.backgroundColorsHover?.[props?.type || "primary"] ||
+      defautTheme.backgroundColorsHover.primary};
+    color: ${(props) =>
+      props?.theme?.colorsHover?.[props?.type || "primary"] ||
+      defautTheme.colorsHover.primary};
   }
 `;
 
 export const Button = (props: ButtonProps) => {
   return (
-    <ThemeProvider>
-      <StyledButton {...props}>{props?.children}</StyledButton>
-    </ThemeProvider>
+    <>
+      {props.children ? (
+        <StyledButton {...props}>{props?.children}</StyledButton>
+      ) : null}
+    </>
   );
 };
